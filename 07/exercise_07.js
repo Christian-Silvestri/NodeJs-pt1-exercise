b706445d36966e167c910c5c66ef6aca38911259
Christian Silvestri <christiansilvestri@live.it>
2022-09-08 18:08:43 +0200

    NodeJs-pt1-exercise_06

---
+++ 07/exercise_07.js
@@ -0,0 +1,10 @@
+const figlet = require('figlet');
+
+figlet('NodeJS', function (error, data) {
+    if (error) {
+        console.log('Something went wrong...');
+        console.dir(error);
+        return;
+    }
+    console.log(data);
+})
