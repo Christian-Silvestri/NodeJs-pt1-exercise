65b10812b67c670faed4b15893231f14779b6c64
Christian Silvestri <christiansilvestri@live.it>
2022-09-08 19:22:48 +0200

    NodeJs-pt1-exercise_09

--- 09/app.test.js
+++ 09/app.test.js
@@ -10,7 +10,8 @@ test("GET /", async () => {
   const response = await request
     .get("/")
     .expect(200)
-  /* .expect("Content-Type", "application/json"); */
+    /* .expect("Content-Type", "application/json"); */
+    .expect('Content-type', 'text/html')
 
 
   /* expect(response.body).toEqual({ location: "Earth" }); */
